2e3293b279201368b430f472e010ce1301604c70
Alex Vanderpot <alex@vanderpot.com>
2022-02-05 17:28:34 -0800

    fix sm install

--- etc/dystopia.sh
+++ etc/dystopia.sh
@@ -14,14 +14,13 @@ function update() {
 
 if [ ! -f "${GAME_DIR}/.installed" ]; then
   update
-  wget -qO- "https://mms.alliedmods.net/mmsdrop/1.11/mmsource-1.11.0-git${METAMOD_VERSION}-linux.tar.gz" | tar xvzf - -C "${GAME_DIR}" &&
+  wget -qO- "https://mms.alliedmods.net/mmsdrop/1.11/mmsource-1.11.0-git${METAMOD_VERSION}-linux.tar.gz" | tar xvzf - -C "${GAME_DIR}/dystopia" &&
     rm "${GAME_DIR}/addons/metamod_x64.vdf"
   # Install Sourcemod (comment out for tournament servers)
-  wget -qO- "https://sm.alliedmods.net/smdrop/1.10/sourcemod-1.10.0-git${SOURCEMOD_VERSION}-linux.tar.gz" | tar xvzf - -C "${GAME_DIR}"
+  wget -qO- "https://sm.alliedmods.net/smdrop/1.10/sourcemod-1.10.0-git${SOURCEMOD_VERSION}-linux.tar.gz" | tar xvzf - -C "${GAME_DIR}/dystopia"
   touch "${GAME_DIR}/.installed"
 else
   echo "Already installed."
-  update
 fi
 
 "${GAME_DIR}/bin/srcds_run.sh" -port "${GAME_PORT}" -clientport "${CLIENT_PORT}" -game "${GAME_DIR}/dystopia" ${GAME_ARGS}
